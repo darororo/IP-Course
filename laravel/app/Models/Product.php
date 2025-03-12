ef27d1ecebc0aa1d9eeee770febe71ca35fb8e05
darororo <daroyem99@gmail.com>
2025-03-12 21:51:38 +0700

    fix(Models/Product): typo when declaring fillable array

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillables = ['name','pricing','category_id'];
+    protected $fillable = ['name','pricing','category_id'];
 
     public function category() {
         return $this->belongsTo(Category::class);
