ed24fb92a8d6e29611bc00dcddad99d40041264e
darororo <daroyem99@gmail.com>
2025-03-26 09:06:44 +0700

    tp7 task 05 fix: create activity observer for Model
    remove order observer and create a general model observer

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Cart;
+use App\Models\Category;
+use App\Models\Customer;
 use App\Models\Order;
+use App\Models\Payment;
+use App\Models\Product;
+use App\Models\Wishlist;
+use App\Observers\ModelActivityObserver;
 use App\Observers\OrderActivityObserver;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +28,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Order::observe(OrderActivityObserver::class);
+        Order::observe(ModelActivityObserver::class);
+        Customer::observe(ModelActivityObserver::class);
+        Category::observe(ModelActivityObserver::class);
+        Cart::observe(ModelActivityObserver::class);
+        Product::observe(ModelActivityObserver::class);
+        Wishlist::observe(ModelActivityObserver::class);
+        Payment::observe(ModelActivityObserver::class);
     }
 }
